69d8d7fdac3d3d9f46089d45f7250b0110637dfb
Dinesh Vasudevan <dvasudevan@babbel.com>
2013-12-17 10:53:49 +0100

    Refactored code

--- app/controllers/user_settings_controller.rb
+++ app/controllers/user_settings_controller.rb
@@ -2,31 +2,34 @@ class UserSettingsController < ApplicationController
   include UserSettings::UserSettingsConcern
 
   def show
-    presenter = UserSettingsPresenter.new UserSettings::Key.find(@current_user.id, params[:key])
-    render json: presenter.as_json
+    render json: create_presenter(:find).as_json
   end
 
   def create
-    presenter = create_presenter
-    render json: presenter.as_json
+    render json: create_presenter(:create).as_json
   end
 
   def create_once
-    presenter = create_presenter(true)
-    render json: presenter.as_json
+    render json: create_presenter(:create, true).as_json
   end
 
   def destroy
-    presenter = UserSettingsPresenter.new UserSettings::Key.destroy(@current_user.id, params[:key])
-    render json: presenter.as_json
+    render json: create_presenter(:destroy).as_json
   end
 
   private
-  def create_presenter(once=false)
-    creation =  if once
-                  UserSettings::Key.create(@current_user.id, params[:key], params[:value])
-                else
-                  UserSettings::Key.create_or_update(@current_user.id, params[:key], params[:value])
+  def create_presenter(type, once=false)
+    creation =  case type
+                when :find
+                  UserSettings::Key.find(@current_user.id, params[:key])
+                when :destroy
+                  UserSettings::Key.destroy(@current_user.id, params[:key])
+                when :create
+                  if once
+                    UserSettings::Key.create(@current_user.id, params[:key], params[:value])
+                  else
+                    UserSettings::Key.create_or_update(@current_user.id, params[:key], params[:value])
+                  end
                 end
 
     UserSettingsPresenter.new creation
